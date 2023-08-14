142dc38bee16fb5c9865924752abb3f42e3eb888
pruebaJavaSyanza <syanza@gmail.com>
2023-08-14 19:32:32 -0400

    avance3

--- SpeechRecognition/BussinessCode/SpeechRecognition.py
+++ SpeechRecognition/BussinessCode/SpeechRecognition.py
@@ -29,6 +29,8 @@ class SpeechBussiness():
         RATE = 44100 #Numero de samples (single data point representing the amplitude of an audio signal at a specific point in time.)
 
         audio = pyaudio.PyAudio()  #crea una instancia de PyAudio para input y output de audio
+        self.message=tk.StringVar()
+        self.message=""
 
         stream = audio.open(format=FORMAT, #abre el archivo para grabar los valores anteriores
                         channels=CHANNELS,
@@ -40,46 +42,61 @@ class SpeechBussiness():
 
         frames = [] #collection of samples, inicialmente vacia y se llena leyendo el stream
 
-        for i in range(0, int(RATE / CHUNK * int(duration))): #RATE / CHUNK = numero de chunks per second and * duration para tener el total de 
-        #chunks per record
-            data = stream.read(CHUNK)  #en cada loop se toma el valor y despues se agrega al frame
-            frames.append(data)
+        try:
 
-        print("Finished recording.")  #finaliza la grabacion
+            for i in range(0, int(RATE / CHUNK * int(duration))): #RATE / CHUNK = numero de chunks per second and * duration para tener el total de 
+            #chunks per record
+                data = stream.read(CHUNK)  #en cada loop se toma el valor y despues se agrega al frame
+                frames.append(data)
 
-        stream.stop_stream() #se detiene el stream
-        stream.close()# se cierra el stream
-        audio.terminate() # finaliza el pyAudio 
+            print("Finished recording.")  #finaliza la grabacion
 
-        root_directory = "./audios_recorded"  # Change this to your desired root directory
-        filename_full = f"{root_directory}/{filename}.wav"
-        print(filename_full)
+            stream.stop_stream() #se detiene el stream
+            stream.close()# se cierra el stream
+            audio.terminate() # finaliza el pyAudio 
 
-        #No se si esto ponerlo en una funcion por si se quiere guardar el audio o si no
+            root_directory = "./audios_recorded"  # Change this to your desired root directory
+            filename_full = f"{root_directory}/{filename}.wav"
+            print(filename_full)
 
-        wf = wave.open(filename_full, 'wb') #utilizamos la libreria WAVE para abrir el audio y en este caso write binary
-        wf.setnchannels(CHANNELS)
-        wf.setsampwidth(audio.get_sample_size(FORMAT))
-        wf.setframerate(RATE)
-        #los 3 anteriores establecen los valores del audio usando funciones del WAVE, todos los set
-        wf.writeframes(b''.join(frames)) #usamos los frames que se incluyeron en la parte de grabar y queda en el audio file
-        #la b es para convertir en bytes antes de escribirlo en el audio
-        wf.close()#se cierra el nuevo wav file
+            #No se si esto ponerlo en una funcion por si se quiere guardar el audio o si no
+
+            wf = wave.open(filename_full, 'wb') #utilizamos la libreria WAVE para abrir el audio y en este caso write binary
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(audio.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            #los 3 anteriores establecen los valores del audio usando funciones del WAVE, todos los set
+            wf.writeframes(b''.join(frames)) #usamos los frames que se incluyeron en la parte de grabar y queda en el audio file
+            #la b es para convertir en bytes antes de escribirlo en el audio
+            wf.close()#se cierra el nuevo wav file
+        
+        except Exception as e:
+                self.message = str(e)
+                print("Error in record_audio")
+
+        return self.message
 
     def audio_to_text(self,filename):
-        r = sr.Recognizer()
+        
+        
         root_directory = "./audios_recorded"  # Change this to your desired root directory
         filen = f"{root_directory}/{filename}"
         print("directory: " + filename)
 
-        #mixer.music.load(filen)
-        #mixer.music.play()
-        #playsound('/path/note.wav')
-        playsound(filen)
-        print('playing sound using  playsound')
+        self.text_play=tk.StringVar()
+        self.text_play="Could not play the audio"
+
+        try:
+            playsound(filen)
+            print('playing sound using  playsound')
+        except Exception as e:
+            self.message = str(e)
+            print("Error in playing sound using playsound: " + str(e))
 
         self.text_recognize=tk.StringVar()
         self.text_recognize="Could not recognize the audio"
+
+        r = sr.Recognizer()
 
         with sr.AudioFile(filen) as source:
             audio_data = r.record(source)
@@ -88,13 +105,14 @@ class SpeechBussiness():
                 print("Text file: " + textFile)
                 self.text_recognize = textFile
 
-            except sr.UnknownValueError:
+            except sr.UnknownValueError as e:
                 print("Could not recognize the audio")
-                self.text_recognize="Could not recognize the audio"
+                print(e)
+                self.text_recognize="Error: Could not recognize the audio"
             
             except sr.RequestError as e:
                 print(e)
-                self.text_recognize="Could not recognize the audio"
+                self.text_recognize="Error: Could not recognize the audio"
 
         #Guardando el registro en la base de datos
         file_blob = self.convert_into_binary(filen)
